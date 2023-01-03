c49ec71da15758d294173205c68a45dcf4f263a8
nguyenvansy1 <ashewarakill2@gmail.com>
2023-01-03 11:01:34 +0700

    .

--- src/main/java/com/example/becapstone1/service/Impl/ExcelServiceImpl.java
+++ src/main/java/com/example/becapstone1/service/Impl/ExcelServiceImpl.java
@@ -52,7 +52,9 @@ public class ExcelServiceImpl {
 
     public ByteArrayInputStream export(List<EventUser> eventUsers) throws IOException {
         String event = "";
-
+        String location = "";
+        String start = "";
+        String end = "";
         String[] columns = {"STT","Code","Name","Time Checkin"};
         try(Workbook workbook = new XSSFWorkbook();
             ByteArrayOutputStream out = new ByteArrayOutputStream())
@@ -61,10 +63,22 @@ public class ExcelServiceImpl {
             Sheet sheet = workbook.createSheet("Event Statistical");
             Row row0 = sheet.createRow(0);
             Cell cell0 = row0.createCell(0);
+            Row row1 = sheet.createRow(1);
+            Cell cell1= row0.createCell(0);
+            Row row2 = sheet.createRow(2);
+            Cell cell2 = row0.createCell(0);
+            Row row3 = sheet.createRow(3);
+            Cell cell3 = row0.createCell(0);
             for (EventUser user: eventUsers) {
                  event = user.getEvent().getName();
+                 location = user.getEvent().getLocation();
+                 start = user.getEvent().getStartTime().toString();
+                 end = user.getEvent().getEndTime().toString();
             }
-            cell0.setCellValue(event);
+            cell0.setCellValue("Event: " + event);
+            cell1.setCellValue("Location: " + location);
+            cell2.setCellValue("Start: " + start);
+            cell3.setCellValue("End: " + end);
             Font headerFont = workbook.createFont();
             headerFont.setBold(true);
             headerFont.setColor(IndexedColors.BLUE.getIndex());
@@ -83,13 +97,13 @@ public class ExcelServiceImpl {
             CellStyle cellStyle1 = workbook.createCellStyle();
             cellStyle1.setDataFormat(creationHelper.createDataFormat().getFormat("#"));
 
-            int rowIndex = 1;
+            int rowIndex = 2;
             for (EventUser user: eventUsers) {
                 Row row = sheet.createRow(rowIndex++);
                 row.createCell(0).setCellValue(rowIndex-1);
                 row.createCell(1).setCellValue(user.getUser().getCode());
                 row.createCell(2).setCellValue(user.getUser().getName());
-                row.createCell(3).setCellValue(user.getCheckin());
+                row.createCell(3).setCellValue(user.getCheckin().toString());
             }
 
             workbook.write(out);
